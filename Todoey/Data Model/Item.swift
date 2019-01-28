1e24c6a01144bb27db5149172707e56d73aa4ea6
Kyle Kotanchek <kj.kotanchek@me.com>
2019-01-28 15:21:09 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kyle Kotanchek on 1/28/19.
+//  Copyright © 2019 Kyle Kotanchek. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
